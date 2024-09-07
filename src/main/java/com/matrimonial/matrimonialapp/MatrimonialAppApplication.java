18dff91c22a20286e21bd33c23dc80050e279068
harissssssss <haris.play123@gmail.com>
2024-09-07 18:19:58 +0530

    starting with code for matrimonial app

---
+++ src/main/java/com/matrimonial/matrimonialapp/MatrimonialAppApplication.java
@@ -0,0 +1,13 @@
+package com.matrimonial.matrimonialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrimonialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrimonialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matrimonial/matrimonialapp/MatrimonialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrimonial.matrimonialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrimonialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
