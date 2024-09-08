e02defef5ca80f59d6f10c46e86df63407b8d2ac
harissssssss <haris.play123@gmail.com>
2024-09-08 18:00:43 +0530

    Adding Log4J and Lombok in pom.xml

--- src/main/java/com/matrimonial/matrimonialapp/MatrimonialAppApplication.java
+++ src/main/java/com/matrimonial/matrimonialapp/MatrimonialAppApplication.java
@@ -1,13 +1,16 @@
 package com.matrimonial.matrimonialapp;
-
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class MatrimonialAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatrimonialAppApplication.class, args);
+		log.info("Matrimonial Application started successfully!");
 	}
 
 }
+
